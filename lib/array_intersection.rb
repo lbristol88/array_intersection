0677de94b881d0c8ab7b6e944c8d7e8411b1b896
Layla Bristol <layla.bristol@gmail.com>
2018-11-15 22:08:11 -0800

    cs fun array intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,18 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  return [] unless array1 && array2
+
+  intersection = []
+
+  array1.each do |i|
+    array2.each do |j|
+      if i == j
+        intersection << j
+
+        break
+      end
+    end
+  end
+
+  return intersection
 end
